faeadbd7dfef1701c9c20fefbad18d9a1dc09c1f
elodie baeza <elodie.baeza@gmail.com>
2021-08-27 13:02:00 +0200

    Change color logo.svg

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
-import logo from 'assets/logo.png'
 import React from 'react'
-import 'styles/index.css';
+import 'styles/index.css'
+import { ReactComponent as Logo } from 'assets/logo.svg'
 
 
 class Footer extends React.Component{
     render(){
         return (
             <footer className='footer-desktop'>
-                <img src={logo} alt="Kasa" className='footer-logo'/>
+                <Logo className='footer-logo'/>
                 <p>© 2020 Kasa. All rights reserved</p>
             </footer>
         );
