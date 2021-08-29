0465ddff002ac614ebea27103048add3e3bc6566
elodie baeza <elodie.baeza@gmail.com>
2021-08-29 18:25:45 +0200

    Create Carrousel

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import 'styles/index.css';
-import Header from 'components/Header';
-import Footer from './Footer';
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Banner from './Banner';
+import Erreur from './Erreur';
+import bannerHome from 'assets/banner-home.jpg';
+import bannerAPropos from 'assets/banner-a-propos.jpg';
+import Tag from './Tag';
+import Thumb from './Thumb';
+import Carrousel from './Carrousel';
 
 function App() {
   return (<React.Fragment>
       <Header />
+      <main>
+        <Thumb title='Titre de la location'/>
+        <Carrousel slideIndex={3} />
+          <Tag name='tag-name'/> 
+        <Banner picture={bannerHome} text='Chez vous, partout et ailleurs'/>
+        <Banner picture={bannerAPropos} />
+        <Erreur />
+      </main>
       <Footer />
     </React.Fragment>
   )
---
+++ src/components/Carrousel.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import 'styles/index.css'
+import arrowBack from 'assets/arrow_back.svg'
+import arrowForward from 'assets/arrow_forward.svg'
+import logements from 'datas/logements.json'
+
+const pictures = logements[1].pictures
+
+class Carrousel extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            setSlideIndex: props.slideIndex
+        }
+        this.prevSlide = this.nextSlide.bind(this)
+        this.nextSlide = this.nextSlide.bind(this)
+    }
+
+    nextSlide() {
+        if(this.state.setSlideIndex !== pictures.length) {
+            this.setState(state => {
+                return {setSlideIndex: state.setSlideIndex + 1}
+            });
+        } 
+        else if (this.state.setSlideIndex === pictures.length)
+        {
+            this.setState({setSlideIndex: 1})
+        }
+    }
+
+    prevSlide() {
+        if(this.state.setSlideIndex !== 1) {
+            this.setState(state => {
+                return {setSlideIndex: state.setSlideIndex - 1}
+            });
+        }
+        if(this.state.setSlideIndex === 1) {
+            this.setState({setSlideIndex: pictures.length})
+        }
+    }
+
+    render() {
+        return (
+            <div className='slider-container'>
+                <div className='slider'>
+                    {pictures.map((src, index) => 
+                        <img 
+                        key={index + 1} 
+                        src={src} alt=""
+                        className={this.state.setSlideIndex === index + 1 ? "active" : "slide" }
+                        />
+                    )}
+                    <p>{this.state.setSlideIndex}/{pictures.length}</p>
+                </div>
+                <div className='slider-buttons'>
+                    <button type='button' onClick={this.prevSlide}>
+                        <img src={arrowBack} alt=''/>
+                    </button>
+                    <button type='button' onClick={this.nextSlide}>
+                        <img src={arrowForward} alt=''/>
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Carrousel
