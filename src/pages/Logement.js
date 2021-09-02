4c92817c24119183ad84e3bf5eff20fc81ce81bc
elodie baeza <elodie.baeza@gmail.com>
2021-09-03 00:12:07 +0200

    Logement page updated

--- src/pages/Logement.js
+++ src/pages/Logement.js
@@ -5,7 +5,7 @@ import Tag from '../components/Tag';
 import Carrousel from '../components/Carrousel';
 import Dropdown from '../components/Dropdown';
 import logementsDatas from 'datas/logements.json'
-
+import Rate from 'components/Rate';
 
 class Logement extends React.Component {
 
@@ -36,10 +36,28 @@ class Logement extends React.Component {
         <React.Fragment>
           <main>
             <Carrousel pictures={this.state.logement.pictures} />
-            {this.state.logement.tags.map( (item, index) =>
-              <Tag key={index} name={item} /> 
-            )}
-            <div>
+            <div className='presentation-container'>
+              <div>
+                <div className='title-container'>
+                  <div className='title'>{this.state.logement.title}</div>
+                  <div className='location'>{this.state.logement.location}</div>
+                </div>
+                <div>
+                  {this.state.logement.tags.map( (item, index) =>
+                    <Tag key={index} name={item} /> 
+                    )}
+                </div>
+              </div>
+              <div>
+                <div className='host-container'>
+                  {this.state.logement.host.name.split(' ')[0]}<br/>
+                  {this.state.logement.host.name.split(' ')[1]}
+                  <img src={this.state.logement.host.picture} alt="host" className='host-picture'/>
+                </div>
+                <Rate value={this.state.logement.rating}/>
+              </div>
+            </div>
+            <div className='dropdowns'>
               <Dropdown title='Equipements' list={this.state.logement.equipments}/>
               <Dropdown title='Description' list={this.descriptionInArray()}/>
             </div>
